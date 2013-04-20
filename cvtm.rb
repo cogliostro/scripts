6bddbd4d9bff3d37eab379d85a1aeef98c0eaa37
Kenneth Vanvik Hansen <kvhansen@online.no>
2013-04-20 15:13:24 +0200

    Fix

--- cvtm.rb
+++ cvtm.rb
@@ -9,6 +9,8 @@ unless ARGV.length == 1
 	exit 1
 end
 
+@torrent = ARGV.first
+
 cd('/home/jkr/Downloads/' + ARGV.first)
 pwd()
 
@@ -16,11 +18,26 @@ pwd()
 
 def moveToVideoDir(torrent)
 	
-	if ( file = Dir['*.rar'])
+	if ( file = Dir['*.rar'].first)
+		puts "Extracting #{file} to #{@cwd}"
 		`unrar x #{file} #{@cwd}`
-		puts "Extracted #{file} to #{@cwd}"
+		logTorrent(file)
+		puts "Done."
 	end
 
+	if ( file = Dir['*.avi'] )
+		puts "Copying #{file} to #{@cwd}"
+		file.each do |fil|
+			`cp #{fil} #{cwd}`
+		end
+		logTorrent(file)
+		puts "Done."
+	end
+
+end
+
+def logTorrent(name)
+	`echo #{name} > /home/jkr/.torrent_history`
 end
 
 
