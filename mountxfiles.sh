07a3f275037b2c4a70d826b8b9ae4cd824a2e243
Kenneth Vanvik <kennethvh@gmail.com>
2015-10-06 21:00:25 +0200

    removed x

--- mountxfiles.sh
+++
@@ -1,2 +0,0 @@
-sshfs -o Ciphers=arcfour -o allow_other -o follow_symlinks -o transform_symlinks kennetvh@xfiles.tihlde.org:/home/staff/kennetvh/filmer /home/xfiles/filmer
-sshfs -o Ciphers=arcfour -o allow_other -o follow_symlinks -o transform_symlinks kennetvh@xfiles.tihlde.org:/home/staff/kennetvh/serier /home/xfiles/serier
